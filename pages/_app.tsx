81802668ca27c8228c36204bdc5d90df6e26d42c
Всеволод <kreepmeister@gmail.com>
2023-06-30 20:03:02 +0300

    🇷🇺 Add localization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,41 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
 import { MantineProvider } from "@mantine/core";
+import { ruRU } from "@clerk/localizations";
 
 export default function App(props: AppProps) {
-  const { Component, pageProps } = props;
+	const { Component, pageProps } = props;
 
-  return (
-    <ClerkProvider>
-      <Head>
-        <title>Page title</title>
-        <link rel="shortcut icon" href="/favicon.svg" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
+	return (
+		<ClerkProvider
+			appearance={{
 
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: "dark",
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
-    </ClerkProvider>
-  );
+				baseTheme: dark
+
+			}}
+			localization={ruRU}
+		>
+			<Head>
+				<title>Page title</title>
+				<link rel="shortcut icon" href="/favicon.svg" />
+				<meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width"
+				/>
+			</Head>
+
+			<MantineProvider
+				withGlobalStyles
+				withNormalizeCSS
+				theme={{
+					/** Put your mantine theme override here */
+					colorScheme: "dark",
+				}}
+			>
+				<Component {...pageProps} />
+			</MantineProvider>
+		</ClerkProvider>
+	);
 }
